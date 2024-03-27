8ecbc7e8c84eb843c8f9e03cc410fc0a62f564fc
Ronaldo Mine <andremine98@gmail.com>
2024-03-28 00:40:38 +0100

    Fixed header menu

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,56 +2,69 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 import { IoMailOutline, IoMenu } from "react-icons/io5";
+import { TiTimes } from "react-icons/ti";
 import { VscGithub } from "react-icons/vsc";
 
 export function Header() {
   const pathname = usePathname();
+  const [visible, setVisible] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  const toogleMenu = () => {
+    setVisible(false)
+  }
+
+  useEffect(() => {
+    function handleClickOutside(event: any) {
+      if (ref.current && !ref.current.contains(event.target) && visible) {
+        toogleMenu();
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ref, toogleMenu]);
 
   return (
-    <header className="py-6">
+    <header className="py-6" id="header">
       <div className="flex justify-between items-center mb-1">
-        <label htmlFor="menu" className="cursor-pointer sm:hidden">
-          <IoMenu className="text-white" size={"28"} />
-        </label>
-        <input
-          type="checkbox"
-          name="menu"
-          id="menu"
-          aria-label="Menu"
-          className="appearance-none peer sm:hidden"
-        />
-        <div className="hidden flex-col gap-6 absolute z-50 top-14 bg-white py-6 px-10 transition-all duration-500 ease-in peer-checked:flex sm:top-0 sm:flex sm:p-0 sm:bg-transparent sm:flex-row sm:gap-10 sm:relative">
+        <button className="cursor-pointer sm:hidden">
+          {
+            visible ? <IoMenu className={`text-white`} size={"28"} onClick={() => setVisible(true)} />
+              : <TiTimes className={`text-white`} size={"28"} onClick={() => setVisible(false)} />
+          }
+        </button>
+        <div ref={ref} className={`hidden flex-col gap-6 absolute z-50 top-14 bg-white py-6 px-10 transition-all duration-500 ease-in ${visible ? "!flex" : ""} sm:top-0 sm:flex sm:p-0 sm:bg-transparent sm:flex-row sm:gap-10 sm:relative`}>
           <Link
             href="/"
-            className={`${
-              pathname === "/"
-                ? "text-red"
-                : "text-pussian-blue sm:text-white"
-            }`}
+            className={`${pathname === "/"
+              ? "text-red"
+              : "text-pussian-blue sm:text-white"
+              }`}
             aria-label="Home Page"
           >
             Home
           </Link>
           <Link
             href="/experience"
-            className={`${
-              pathname === "/experience"
-                ? "text-red"
-                : "text-pussian-blue sm:text-white"
-            }`}
+            className={`${pathname === "/experience"
+              ? "text-red"
+              : "text-pussian-blue sm:text-white"
+              }`}
             aria-label="Experience Page"
           >
             Experience
           </Link>
           <Link
             href="/about"
-            className={`${
-              pathname === "/about"
-                ? "text-red"
-                : "text-pussian-blue sm:text-white"
-            }`}
+            className={`${pathname === "/about"
+              ? "text-red"
+              : "text-pussian-blue sm:text-white"
+              }`}
             aria-label="About Page"
           >
             About
